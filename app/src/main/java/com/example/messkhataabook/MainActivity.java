18a7e7caaecf18dfddd2f65d390cfc3de5bf6ce0
KARAN KALRA <karankalra047@gmail.com>
2022-03-12 11:27:36 -0800

    Calendar in java

--- app/src/main/java/com/example/messkhataabook/MainActivity.java
+++ app/src/main/java/com/example/messkhataabook/MainActivity.java
@@ -1,14 +1,29 @@
 package com.example.messkhataabook;
 
 import androidx.appcompat.app.AppCompatActivity;
+import java.util.Calendar;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button= findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Calendar calendar= Calendar.getInstance();
+                Toast.makeText(MainActivity.this, "Time and Date:"+calendar.getTime(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
+
 }
